b938188177b2a6aaf394cf4d54cd3d8a617f1226
vmarquez008 <46207132+vmarquez008@users.noreply.github.com>
2020-10-28 20:36:04 -0600

    Add files via upload

---
+++ Sesion 02.sql
@@ -0,0 +1,8 @@
+SHOW DATABASE;
+USE tienda;
+SHOW Tables;
+SELECT *  FROM articulo WHERE nombre LIKE "%Pasta%";
+SELECT *  FROM articulo WHERE nombre LIKE "%Cannelloni%";
+SELECT *  FROM articulo WHERE nombre LIKE "% - %";
+SELECT *  FROM puesto WHERE nombre LIKE "%Designer%";
+SELECT *  FROM puesto WHERE nombre LIKE "%Developer%";
